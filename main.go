4006d362ce9591cc06e05b8e36763164021aaf9c
rossi <ramiromrossi@gmail.com>
2026-09-06 01:45:24 -0300

    added support for custom 404 page and css files

--- main.go
+++ main.go
@@ -1,23 +1,57 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
 func main() {
 	staticDir := "./static"
-
 	fileServer := http.FileServer(http.Dir(staticDir))
 
-	http.Handle("/", fileServer)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		path := filepath.Join(staticDir, filepath.Clean(r.URL.Path))
+
+		info, err := os.Stat(path)
+		if os.IsNotExist(err) {
+			serve404(w, staticDir)
+			return
+		}
+		if err != nil {
+			log.Printf("error al acceder a %s: %v", path, err)
+			http.Error(w, "error interno del servidor", http.StatusInternalServerError)
+			return
+		}
+
+		if info.IsDir() && r.URL.Path != "/" {
+			indexPath := filepath.Join(path, "index.html")
+			if _, errIndex := os.Stat(indexPath); os.IsNotExist(errIndex) {
+				serve404(w, staticDir)
+				return
+			}
+		}
+
+		fileServer.ServeHTTP(w, r)
+	})
 
 	port := ":8080"
-	fmt.Printf("Servidor estatico escuchano en http://localhost%s\n", port)
-	fmt.Printf("Sirviendo archios desde: %s\n", staticDir)
+	log.Printf("Servidor estatico escuchando en http://localhost%s\n", port)
+	log.Printf("Sirviendo archivos desde: %s\n", staticDir)
 
-	err := http.ListenAndServe(port, nil)
-	if err != nil {
-		fmt.Printf("Error al iniciar el servidor: %s\n", err)
+	if err := http.ListenAndServe(port, nil); err != nil {
+		log.Fatalf("Error al iniciar el servidor: %s\n", err)
 	}
+}
+
+func serve404(w http.ResponseWriter, staticDir string) {
+	content, err := os.ReadFile(filepath.Join(staticDir, "404.html"))
+	if err != nil {
+		http.Error(w, "404 page not fond", http.StatusNotFound)
+		return
+	}
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusNotFound)
+	w.Write(content)
 }
